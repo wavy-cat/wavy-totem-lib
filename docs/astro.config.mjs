d1d0dca096f49dc7c85a0baa5cb3d2f99a198825
WavyCat <root@wavycat.me>
2026-05-25 21:26:37 +0500

    docs: update favicon to use .png format

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     integrations: [
         starlight({
             title: 'TotemLib',
-            favicon: 'favicon.ico',
+            favicon: 'favicon.png',
             defaultLocale: 'en',
             locales: {
                 en: {
